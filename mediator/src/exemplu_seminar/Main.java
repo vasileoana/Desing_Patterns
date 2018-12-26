a55a6658a42b03d23d34eb7da225027bcc894436
Oana <vasileoana22@gmail.com>
2018-12-27 00:30:26 +0200

    mediator

--- mediator/src/exemplu_seminar/Main.java
+++ mediator/src/exemplu_seminar/Main.java
@@ -1,5 +1,5 @@
 package exemplu_seminar;
-
+//Simplificarea comunicării prin eliminarea relațiilor de tip mulți la mulți și transformarea acestora în relații de tip unu la mulți și mulți la unu
 public class Main {
     public static void main(String[] args) {
 
