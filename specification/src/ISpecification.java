bd0ea7b64f792e02f3e365f249c3afb23737f499
Oana <vasileoana22@gmail.com>
2018-12-27 19:32:44 +0200

    specification

--- specification/src/ISpecification.java
+++
@@ -1,8 +0,0 @@
-public interface ISpecification {
-    boolean isSatisfiedBy(Object candidate);
-    ISpecification And(ISpecification other);
-    ISpecification AndNot(ISpecification other);
-    ISpecification Or(ISpecification other);
-    ISpecification OrNot(ISpecification other);
-    ISpecification Not();
-}
